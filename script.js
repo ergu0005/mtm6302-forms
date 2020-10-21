6c2623e0c00e7522cbc623036fdf7bc80cde9afc
ergu0005 <ergu0005@algonquinlive.com>
2020-10-21 14:47:47 -0400

    en

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const $form = document.getElementById('main-form')
 
-$form.addEventListener('submit', function (event){
-console.log('respond to form')
+$form.addEventListener('submit', function (event) {
 
-}
+console.log('$form.elements[0].value)
+
+event.preventDefult()
+})
